648b9af240777e7894adefa8058c76943f71de43
Pradyumna <pradyumnad092004@gmail.com>
2022-11-09 23:22:50 +0530

    Added todo-list webapp

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
-const taskInput = document.querySelector(".
-");
-// TODO: select the input with class task-input
-const addTaskBtn = document.querySelector(".task-add-btn");
-// TODO: select the button with class task-add-btn
-const taskElements = document.querySelector(".task-elements");
-// TODO: select the div with class task-elements
+const taskInput = document.querySelector(".task-input");
 
-let taskCount = 1;
+const addTaskBtn = document.querySelector(".task-add-btn");
+
+const taskElements = document.querySelector(".task-elements");
+
+let taskCount = 0;
 
 function addTask() {
-  taskId = `task-${++taskCount}`;
-  taskIdDiv = `task-${taskId}-div`;
+  if (taskInput.value !== "") {
+    taskId = `task-${++taskCount}`;
+    taskIdDiv = `task-${taskId}-div`;
 
-  const taskDiv = document.createElement("div");
-  taskDiv.classList.add("task-element");
-  // TODO: create <div class="task-element"></div>
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-element");
+    // TODO: create <div class="task-element"></div>
 
-  const taskColl = document.createElement("div");
-  taskColl.id = taskIdDiv;
+    const taskColl = document.createElement("div");
+    taskColl.id = taskIdDiv;
+    taskColl.classList.add("task-coll");
 
-  const taskCheckbox = document.createElement("input");
-  taskCheckbox.type = "checkbox";
-  taskCheckbox.id = taskId;
-  taskCheckbox.classList.add("task-checkbox");
-  taskCheckbox.addEventListener('click', setTaskComplete());
-  // TODO: create <input class="task-checkbox" type="checkbox" id=taskId />
+    const checkBoxDiv = document.createElement("div");
 
-  const taskLabel = document.createElement("label");
-  taskLabel.setAttribute("for", taskId);
-  taskLabel.innerHTML = taskInput.value;
-  // TODO: create <label for=taskId>task</label>
+    const taskCheckbox = document.createElement("input");
+    const taskLabel = document.createElement("label");
+    const deleteBtn = document.createElement("button");
 
-  const taskElement = document.querySelector(".task-element");
-  taskColl.appendChild(taskCheckbox);
-  taskColl.appendChild(taskLabel);
-  taskElement.appendChild(taskColl);
-  // TODO: add taskCheckbox & taskLabel to <div class="task-element"></div>
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.addEventListener("click", (event) => {
+      taskColl.remove();
+      console.log("DELETE!");
+    });
 
-  taskElements.appendChild(taskElement);
+    taskCheckbox.type = "checkbox";
+    taskCheckbox.id = taskId;
+    taskCheckbox.classList.add("task-checkbox");
+    taskCheckbox.addEventListener("click", (event) => {
+      if (taskCheckbox.checked) {
+        taskLabel.classList.add("task-done");
+      } else {
+        taskLabel.classList.remove("task-done");
+      }
+      setTaskComplete();
+    });
 
-  taskInput.value = "";
-  // TODO add <div class="task-element">...</div> to <div class="task-elements">...</div>
-}
+    taskLabel.setAttribute("for", taskId);
+    taskLabel.innerHTML = taskInput.value;
 
-// TODO: set a task as complete by crossing out the label by including task-done class
-function setTaskComplete() {
-  console.log("YEA BUDDY!");
+    const taskElement = document.querySelector(".task-element");
+    checkBoxDiv.appendChild(taskCheckbox);
+    checkBoxDiv.appendChild(taskLabel);
+    taskColl.appendChild(checkBoxDiv);
+    taskColl.appendChild(deleteBtn);
+    taskElement.appendChild(taskColl);
+
+    taskElements.appendChild(taskElement);
+
+    taskInput.value = "";
+  }
 }
 
 addTaskBtn.addEventListener("click", (event) => {
   addTask();
 });
-
-
-// TODO: add event listener to task-add-btn
-
-// TODO: add event listener to task-checkboxes
-function addListenerToCheckboxes() {}
